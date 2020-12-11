fd6784b8b111ff2be1e1c84c88746ea59f44a38d
Saksham Aggarwal <sakshamaggarwal@Sakshams-MacBook-Pro.local>
2020-12-11 02:44:58 -0800

    specific driver completed orders handler

--- servers/microservice/handlers/driver/driverSpecificHandlers.js
+++ servers/microservice/handlers/driver/driverSpecificHandlers.js
@@ -1,5 +1,5 @@
 
-// Handles GET request for a specific driver
+// Handles GET request for a specific driver to get assigned orders
 const getAssignedOrdersHandlers = async (req, res, { Order }) => {
     let authUser = JSON.parse(req.get("X-User"));
     if (!authUser) {
@@ -25,6 +25,26 @@ const getAssignedOrdersHandlers = async (req, res, { Order }) => {
         }
         res.setHeader("Content-Type", "application/json");
         res.status(200).json(incompleteOrders);
+    } catch (e) {
+        res.status(500).send("Internal Server Error - " + e);
+        return;
+    }
+}
+
+// Handles GET request for a specific driver to get completed orders
+const getAssignedOrdersHandlers = async (req, res, { Order }) => {
+    let authUser = JSON.parse(req.get("X-User"));
+    if (!authUser) {
+        res.status(401).send("Unauthorized user")
+        return;
+    }
+
+    var driverID = authUser.id;
+
+    try {
+        const completedOrders = await Order.find( { driverID: driverID, status: "Completed" });
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(completedOrders);
     } catch (e) {
         res.status(500).send("Internal Server Error - " + e);
         return;
